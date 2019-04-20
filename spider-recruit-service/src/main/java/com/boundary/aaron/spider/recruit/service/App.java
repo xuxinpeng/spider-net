600cff1b4a942832f42411ddb31c6499a97df144
benxiao <benxiao@wacai.com>
2019-04-20 14:14:33 +0800

    增加文件

---
+++ spider-recruit-service/src/main/java/com/boundary/aaron/spider/recruit/service/App.java
@@ -0,0 +1,13 @@
+package com.boundary.aaron.spider.recruit.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
